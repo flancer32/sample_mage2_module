9357fcb8c6c4ffe41f02d497a4fbeeaa32141320
Alex Gusev <alex@flancer64.com>
2015-10-14 20:16:03 +0300

    MOBI-101 - Create repos for critical path

--- test/unit/phpunit_bootstrap.php
+++ test/unit/phpunit_bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 /**
  * Include composer autoloader.
- * Path is relative to "./work/vendor/flancer32/sample_magelib_demo/test/unit/" folder.
+ * Path is relative to "./work/vendor/<vendor>/<module>/test/unit/" folder.
  */
 include_once(__DIR__ . '/../../../../../vendor/autoload.php');
