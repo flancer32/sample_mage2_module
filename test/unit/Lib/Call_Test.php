192a640dd1822e1082089f524cbccc9385d345d5
Alex Gusev <alex@flancer64.com>
2016-02-16 11:55:04 +0200

    MOBI-195 - Upgrade M2 up to 2.0.2

--- test/unit/Lib/Call_Test.php
+++ test/unit/Lib/Call_Test.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Unit test for module's class.
+ * Unit test for module's class. All external dependencies are mocked.
  *
  * User: Alex Gusev <alex@flancer64.com>
  */
@@ -10,21 +10,42 @@ include_once(__DIR__ . '/../phpunit_bootstrap.php');
 
 class Call_Test extends \PHPUnit_Framework_TestCase {
 
-    /**
-     * Call to M2 class that calls to common library method.
-     */
-    public function test_doCall() {
-        $mockCall = $this
-            ->getMockBuilder('Flancer32\Lib\Service\Customer\Call')
+    public function test_doInsert() {
+        // Test Data
+        $TABLE = 'table name';
+        $ID_INSERTED = 21;
+        // Mocks
+        $mResource = $this
+            ->getMockBuilder('Magento\Framework\App\ResourceConnection')
             ->disableOriginalConstructor()
             ->getMock();
-        $mockCall
+        $mConn = $this
+            ->getMockBuilder('Magento\Framework\DB\Adapter\Pdo\Mysql')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        // $this->_conn = $resource->getConnection();
+        $mResource
             ->expects($this->once())
-            ->method('operation')
-            ->will($this->returnValue(34));
-        /** @var  $m2 \Flancer32\Sample\Lib\Call */
-        $m2 = new \Flancer32\Sample\Lib\Call($mockCall);
-        $resp = $m2->doCall();
-        $this->assertEquals(34, $resp);
+            ->method('getConnection')
+            ->will($this->returnValue($mConn));
+        // $tbl = $this->_resource->getTableName(Account::ENTITY_NAME);
+        $mResource
+            ->expects($this->once())
+            ->method('getTableName')
+            ->willReturn($TABLE);
+        // $this->_conn->insert($tbl, $bind);
+        $mConn
+            ->expects($this->once())
+            ->method('insert');
+        // $result = $this->_conn->lastInsertId($tbl);
+        $mConn
+            ->expects($this->once())
+            ->method('lastInsertId')
+            ->willReturn($ID_INSERTED);
+        /** @var  $call \Flancer32\Sample\Lib\Call */
+        $call = new \Flancer32\Sample\Lib\Call($mResource);
+        $resp = $call->doInsert();
+        $this->assertEquals($ID_INSERTED, $resp);
     }
 }
