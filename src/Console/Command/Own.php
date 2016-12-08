e3bde97cb313ad4b56ff8a69a0bcf9dda502e737
Alex Gusev <alex@flancer64.com>
2016-12-08 16:32:45 +0200

    Proxy classes in ObjectManager

---
+++ src/Console/Command/Own.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+namespace Flancer32\Sample\Console\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Own
+    extends \Symfony\Component\Console\Command\Command
+{
+    /** @var \Magento\Quote\Api\CartManagementInterface */
+    protected $cartManagement;
+
+    public function __construct(
+        \Magento\Quote\Api\CartManagementInterface\Proxy $cartManagement
+    ) {
+        parent::__construct('sample:command');
+        $this->cartManagement = $cartManagement;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln("I'm a sample.");
+    }
+
+}
